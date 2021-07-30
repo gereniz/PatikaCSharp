b922a47b504230315a4c4c74125f59987b4c3308
gereniz <gustagereniz03@gmail.com>
2021-07-30 17:52:08 +0300

    1

---
+++ console_programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name);
+        }
+    }
+}
