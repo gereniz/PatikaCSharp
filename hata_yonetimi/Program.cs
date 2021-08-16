791a61e0a7e7bc11d2ccfe0a14057b3d28f06415
gereniz <gustagereniz03@gmail.com>
2021-08-16 14:41:05 +0300

    5

---
+++ hata_yonetimi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               Console.WriteLine("Bir sayı giriniz :");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı : " + sayi);
+         
+            }
+            catch(Exception e){
+                Console.WriteLine("Hata : " + e.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem bitti");
+            }
+
+            // Hata Örnekleri 
+            try
+            {
+                //int a = int.Parse(null);
+                //int b = int.Parse("Text");
+                int c = int.Parse("-200000000000000");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(e);
+            }
+            catch(FormatException e){
+                Console.WriteLine("Veri tipi uygun değildir.");
+                Console.WriteLine(e);
+            }
+            catch(OverflowException e){
+                Console.WriteLine("Sınırı aşan değer girdiniz");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
