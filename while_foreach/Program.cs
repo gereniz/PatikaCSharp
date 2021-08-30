22d1574fd950632751b017ea59232dc4d68a2860
gereniz <gustagereniz03@gmail.com>
2021-08-30 17:43:55 +0300

    10

---
+++ while_foreach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 den başlayarak console den girilen sayıya kdar olan sayıların toplamı
+            Console.Write("Bir sayı giriniz : ");
+            int number = int.Parse(Console.ReadLine());
+            int count = 1;
+            int sum = 0;
+            while (count <= number)
+            {
+                sum+=count;
+                count++;
+            }
+            Console.WriteLine("Ortalama : " + (sum/number));
+
+            // 'a'dan 'z'ye kadar olan harfleri yazdır
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character + " ");
+                character++;
+            }
+            Console.WriteLine("---------");
+
+            string[] cars = {"BMW","Ford","Toyoto","Nissan"};
+            foreach(var car in cars){
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
