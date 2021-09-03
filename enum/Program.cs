74d64405a89a71777ac9f9512678a7f23b919902
gereniz <gustagereniz03@gmail.com>
2021-09-03 17:06:40 +0300

    28

---
+++ enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine((int) Days.Tuesday);
+
+            int heat = 32;
+            if(heat <= (int) Weather.Normal){
+                Console.WriteLine("Dışarı çıkmak için havanın ısınması bekle");
+            }else if(heat >= (int)Weather.Hot){
+                 Console.WriteLine("Dışarı çıkmak için hava sıcak");
+            }else if(heat>=(int)Weather.Normal && heat<=(int)Weather.Normal){
+                 Console.WriteLine("Hadi dışarı çıkalım");
+            }
+
+        }
+    }
+
+    enum Days{
+        Monday = 1,
+        Tuesday, //2
+        Wednesday, //3
+        Thursday, //4
+        Friday = 21, 
+        Saturday,  //22
+        Sunday   //23
+    }
+
+    enum Weather{
+
+        Cold = 5,
+        Normal =20,
+        Hot = 30,
+        VeryHot = 40
+    }
+}
---
+++ struct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.longShort = 4;
+            rectangle.shortEdge = 3;
+
+            Console.WriteLine("Class Alan Hesabı : " + rectangle.CalculateAres());
+
+            RectangleStruct rectangleStruct = new RectangleStruct();
+            rectangleStruct.longShort = 4;
+            rectangleStruct.shortEdge = 3;
+
+            Console.WriteLine("Struct Alan Hesabı : " + rectangleStruct.CalculateAres());
+        }
+    }
+
+    class Rectangle{
+
+        public int shortEdge;
+        public int longShort;
+
+
+        public long CalculateAres(){
+            return this.shortEdge * this.longShort;
+        }
+    }
+
+    struct RectangleStruct{
+
+        public int shortEdge;
+        public int longShort;
+
+
+        public long CalculateAres(){
+            return this.shortEdge * this.longShort;
+        }
+    }
+}
